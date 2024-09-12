891ff9246f0d73c17f61d7e1339ee2ed931c9150
DAlexandress <douglas.alexandre1@icloud.com>
2024-09-12 13:52:41 -0300

    Feat: feat: functional site with links to 'a' tags.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function onClickScrollTo(id) {
   document.getElementById(id).scrollIntoView();
 }
+
+function onClickLink(url) {
+  window.open(url);
+}
+
+function redirectToWhatsApp() {
+  const phoneNumber = "5521971543501"; // Coloque o número de telefone aqui no formato internacional
+  const whatsappURL = `https://wa.me/${phoneNumber}`;
+
+  window.open(whatsappURL);
+}
